b2acb4fc0c772bdd57c1b2e038f4125c0208d127
nvardharutyunyan <nvard.harutyunyan.98@mail>
2018-05-21 15:14:44 -0700

    Python (1st homework)

---
+++ nvard/python/fib.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+a = 0
+b = 1
+print (a)
+while b < 50:
+    print (b)
+    b = a + b
+    a = b - a
---
+++ nvard/python/first_last.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+a = input ("Enter the text : ")
+print(a[-1] + a[1:-1] + a[0])
