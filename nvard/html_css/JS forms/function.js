73e0d6f3adc112c8aa728415b03ca21714ee013e
nvardharutyunyan <nvard.harutyunyan.98@mail>
2018-06-25 19:05:40 -0700

    JS forms

--- nvard/html_css/JS forms/function.js
+++ nvard/html_css/JS forms/function.js
@@ -1,25 +1,30 @@
-function to_realize (F) { 
-    var gender; 
-    if(document.getElementById('gender_Male').checked) {
-        gender = "Mr.";
-    }else if(document.getElementById('gender_Female').checked) {
-        gender = "Mrs.";
+function to_realize (form) { 
+    var mr_mrs; 
+    if(document.getElementById('male').checked) {
+        mr_mrs = "Mr.";
+    }else if(document.getElementById('female').checked) {
+        mr_mrs = "Mrs.";
     }
-    var str = [ gender, F ['Name'].value,F ['Lastname'].value ].join('  ');
-    mt = document.getElementById ('tool');
-    mt.innerHTML = str;
-    mc   = document.getElementById ('modal');
-    mc.style.display = 'block';
-    form = document.getElementById ('form1');
-    form.style.display = 'none';
+    var str = [ 'Hi', mr_mrs, form['Name'].value,form['Lastname'].value, '!' ].join('  ');
+    document.getElementById ('head').innerHTML = str;
+    document.getElementById ('form').style.display = 'none';
+    document.getElementById ('container-form').style.display = 'none';
+    second_page = document.getElementById ('second_page');
+    second_page.style.display = 'block';
 }
-var span = document.getElementsByClassName("continue")[0];
-    span.onclick = function() {
-    document.getElementById ('modal').style.color.red;
-    fromyou = document.getElementById ('fromyou');
-    fromyou.style.display = 'block';
+
+function info_cart(form_data) {
+    var table ="<table ><tr><td> Name: </td> <td>";
+    table += form_data["name"].value +
+            "</td></tr><tr><td>Surname: </td> <td>"+ form_data["surname"].value +
+            "</td></tr><tr><td>Adress: </td> <td>"+ form_data["adress"].value +
+            "</td></tr><tr><td>Country: </td> <td>"+ form_data["country"].value +
+            "</td></tr><tr><td>Phone: </td> <td>"+ form_data["phone"].value + 
+            "</td></tr><tr><td>E-mail: </td> <td>"+ form_data["email"].value + 
+            "</td></tr> </table>";
+    document.getElementById("container_cart").innerHTML += table;
+    for (var i = 0; i < 6; i++) {
+        form_data[i].value = "";  
+    };
 }
-function foo(G) {
-    var info = [[],[],[],[],[],[]].join('<br>');
-    
-}
+
